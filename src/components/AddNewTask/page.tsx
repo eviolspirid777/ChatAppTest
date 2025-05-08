1e975a108787d94666fbc99e97e8fec0fa070b68
eviolspirid <eviolspirid@gmail.com>
2025-05-08 21:27:35 +0300

    Добавлена обработка ошибок при добавлении пустого значения

--- src/components/AddNewTask/page.tsx
+++ src/components/AddNewTask/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Input } from "antd";
+import { Button, Form, Input } from "antd";
 import { type FC, useState } from "react";
 
 type AddNewTaskProps = {
@@ -7,22 +7,35 @@ type AddNewTaskProps = {
 
 export const AddNewTask: FC<AddNewTaskProps> = ({ onAddTask }) => {
   const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   const addTask = () => {
-    const title = inputValue;
-    if (title.trim()) {
-      onAddTask(title);
-      setInputValue("");
+    const title = inputValue.trim();
+    if (!title) {
+      setError("Поле не может быть пустым!");
+      return;
     }
+    onAddTask(title);
+    setInputValue("");
+    setError(null);
   };
 
   return (
     <div className="flex flex-row gap-2">
-      <Input
-        placeholder="Введите задачу"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
+      <Form.Item
+        style={{ marginBottom: 0 }}
+        validateStatus={error ? "error" : ""}
+        help={error}
+      >
+        <Input
+          placeholder="Введите задачу"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            if (error) setError(null);
+          }}
+        />
+      </Form.Item>
       <Button type="primary" onClick={addTask}>
         Добавить
       </Button>
